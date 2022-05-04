98ff47a5d9b8c12662469bc2df18856dc5217515
Deshmukh P <deshmukhpurushothaman@gmail.com>
2022-05-04 08:55:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(innerWidth, innerHeight)
+renderer.setPixelRatio(devicePixelRatio)
+document.body.appendChild(renderer.domElement)
+
+camera.position.z = 5;
+
+const planeGeometry = new THREE.PlaneGeometry( 5, 5, 10, 10 );
+const planeMaterial = new THREE.MeshPhongMaterial({color: 0xff0000, side: THREE.DoubleSide})
+
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
+scene.add(planeMesh)
+
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set( 0, 0, 1)
+scene.add(light)
+
+function animate() {
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+    //planeMesh.rotation.x +=0.01;
+}
+
+animate()
